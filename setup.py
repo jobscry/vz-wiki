87dd3e9c3511af10c8295447c92048e6382d5b87
Joe Vasquez <joe.vasquez@gmail.com>
2009-09-30 22:51:34 -0400

    fixing error in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import ez_setup
 ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(
-    name = "django-vz-testmaker",
+    name = "django-vz-wiki",
     version = "0.1",
     packages = find_packages(),
     author = "Joe Vasquez",
