771ace42e10ea89a4977f6218e5953498f4d1281
Joe Vasquez <joe.vasquez@gmail.com>
2009-10-01 00:18:23 -0400

    more errors in setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(
     name = "django-vz-wiki",
-    version=__import__('django_vz_wiki').__version__
-    packages = find_packages(),
+    version=__import__('django_vz_wiki').__version__,
+    packages=find_packages(exclude=['ez_setup']),
     author = "Joe Vasquez",
     author_email = "joe.vasquez@gmail.com",
     license='BSD',
     description = "A Django powered wiki app",
     long_description=open('README.markdown').read(),
-    url = "http://jobscry.net/projects/#vz_wiki"
+    url = "http://jobscry.net/projects/#vz_wiki",
     download_url = "http://github.com/jobscry/vz-wiki",
     include_package_data = True
 )
