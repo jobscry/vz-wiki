80b0c3e0db501e681b2e67519e225613d9796ffc
Joe Vasquez <joe.vasquez@gmail.com>
2009-10-01 00:16:37 -0400

    added info to setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(
     name = "django-vz-wiki",
-    version = "0.1",
+    version=__import__('django_vz_wiki').__version__
     packages = find_packages(),
     author = "Joe Vasquez",
     author_email = "joe.vasquez@gmail.com",
+    license='BSD',
     description = "A Django powered wiki app",
-    url = "http://github.com/jobscry/vz-wiki",
+    long_description=open('README.markdown').read(),
+    url = "http://jobscry.net/projects/#vz_wiki"
+    download_url = "http://github.com/jobscry/vz-wiki",
     include_package_data = True
 )
