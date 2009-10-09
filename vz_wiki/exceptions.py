1307da7d06c8a3e2881f766c34795f685ebf29ed
joe <joe@vz-laptop.(none)>
2009-10-09 11:17:38 -0400

    cleaned up exceptions.py according to PEP 8

--- vz_wiki/exceptions.py
+++ vz_wiki/exceptions.py
@@ -2,17 +2,24 @@ class PageAlreadyCheckedOut(Exception):
     "Paged check out, please check it in"
     pass
 
+
 class UnpublishedRevisionExists(Exception):
-    "This page has an unpublished revision, please publish or discard any drafts"
+    """
+    This page has an unpublished revision, please publish or discard any
+    drafts
+    """
     pass
+
 
 class AlreadyPublishedRevision(Exception):
     "This revision has already been published, this cannot be undone."
     pass
 
+
 class ComparingSameRevision(Exception):
     "Revision numbers being compared are equal and cannot be compared."
     pass
+
 
 class RevisionDoesNotExist(Exception):
     "Cannot compare revisions because one or more revisions do(es) not exist."
