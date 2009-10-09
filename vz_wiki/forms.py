10dd569a7e082ef1a7ff6e5f647e5bf936156b86
joe <joe@vz-laptop.(none)>
2009-10-09 11:19:07 -0400

    cleaned up forms.py according to PEP 8

--- vz_wiki/forms.py
+++ vz_wiki/forms.py
@@ -2,13 +2,17 @@ from django.forms import ModelForm
 from tagging.forms import TagField
 from models import Page, Revision
 
+
 class PageForm(ModelForm):
+
     class Meta:
         model = Page
         exclude = ['creator', 'is_checked_out']
 
+
 class RevisionForm(ModelForm):
     tags = TagField(label='Tags for Page')
+
     class Meta:
         model = Revision
         exclude = ['page', 'author']
